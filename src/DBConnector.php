87fdb7c32e15b9d037c45780b6c10e12a60721f6
LallemandE <job@lallemand.lu>
2018-03-12 10:27:09 +0100

    DBconnection Service creation

---
+++ src/DBConnector.php
@@ -0,0 +1,45 @@
+<?php
+namespace Service;
+
+class DBConnector {
+    private static $config;
+    private static $connection;
+    
+    /**
+     * setConfig
+     * 
+     * Store the give configuration which includes 
+     * hostname, driver, dbname, dbuser and password.
+     * 
+     * @param array $config the configuration to store
+     * 
+     * @return void
+     */    
+        
+    public static function setConfig(array $config){
+        self::$config = $config;
+    }
+    
+    
+    /**
+     * createConnection
+     * 
+     * Creates a PDO connection using the previously registered configuration
+     * 
+     * @param none
+     * 
+     * @return void
+     */
+    
+    private static function createConnection(){
+        $dsn = sprintf('%s:host=%s;dbname=%s',
+            self::$config['driver']),
+            self::$config['hostname'],
+            self::$config['dbname']);
+            
+            
+            
+       self::$connection = new \PDO($dsn, self::$config['dbuser'], self::$config['dbpass']);     
+    }
+    
+}
