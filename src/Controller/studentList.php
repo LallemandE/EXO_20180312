1afa889883228a1cfbfaa5db13a75c01c7a07c21
LallemandE <job@lallemand.lu>
2018-03-20 09:01:19 +0100

    add of read, modify, delete icon in student summary

--- src/Controller/studentList.php
+++ src/Controller/studentList.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>Logout</title>
+    
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>    
+    
 </head>
 <body>
 
@@ -12,6 +16,13 @@
 
 use Model\Student;
 
+$levelCorrespondance = [
+                        Student::LEVEL_NORMAL => "NORMAL",
+                        Student::LEVEL_GOOD => "GOOD",
+                        Student::LEVEL_SUPER => "VERY GOOD",
+    
+                        ];
+
 $myStudent = new Student();
 $myStudentArray = $myStudent->getAll();
 
@@ -19,15 +30,17 @@ if (count($myStudentArray)){
     echo '<table>';
     echo '<thead>';
     echo '<tr>';
-    echo '<th>Student</th><th>ID</th><th>Level</th>';
+    echo '<th>Student</th><th>Level</th><th>Read</th><th>Modify</th><th>Delete</th>';
     echo '</tr>';
     echo '</thead>';
     foreach($myStudentArray as $studname => $stud){
         echo '<tr>';
         echo "<td>$studname</td>";
-        echo '<td>' . $stud['id'] . '</td>';
-        echo '<td>' . $stud['level'] . '</td>';
-        
+//        echo '<td>' . $stud['id'] . '</td>';
+        echo '<td>' . $levelCorrespondance[$stud['level']] . '</td>';
+        echo '<td><i class="fas fa-eye"></i></td>';
+        echo '<td><i class="fas fa-pencil-alt"></i></td>';
+        echo '<td><i class="fas fa-trash-alt"></i></td>';
         echo '</tr>';
     }
     echo'</table>';
