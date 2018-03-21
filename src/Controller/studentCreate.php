425e974345d39c24f19fcbbf195e64d3e7c813fb
LallemandE <job@lallemand.lu>
2018-03-21 22:37:20 +0100

    student create step 2

--- src/Controller/studentCreate.php
+++ src/Controller/studentCreate.php
@@ -16,13 +16,14 @@ $levelCorrespondance = [
     
     
     <FORM method="POST">
-    <input type="text" id="studentName" name="studentName" placeholder="Student name ? ..." >
-    <select name="level">
-		<option value="<?php echo Student::LEVEL_NORMAL; ?>"><?php echo $levelCorrespondance[Student::LEVEL_NORMAL]; ?></option>    	
-		<option value="<?php echo Student::LEVEL_GOOD; ?>"><?php echo $levelCorrespondance[Student::LEVEL_GOOD]; ?></option>
-		<option value="<?php echo Student::LEVEL_SUPER; ?>"><?php echo $levelCorrespondance[Student::LEVEL_SUPER]; ?></option>
-    </select>
-    <button type="submit">CREATE</button>
+        <input type="text" id="studentName" name="studentName" placeholder="Student name ? ..." >
+        <select name="level">
+    		<option value="<?php echo Student::LEVEL_NORMAL; ?>"><?php echo $levelCorrespondance[Student::LEVEL_NORMAL]; ?></option>    	
+    		<option value="<?php echo Student::LEVEL_GOOD; ?>"><?php echo $levelCorrespondance[Student::LEVEL_GOOD]; ?></option>
+    		<option value="<?php echo Student::LEVEL_SUPER; ?>"><?php echo $levelCorrespondance[Student::LEVEL_SUPER]; ?></option>
+        </select>
+        <button type="submit">CREATE</button>
+        <button onclick="location.href='../index.php'" type="button">CANCEL</button>
     </FORM>
     
     
